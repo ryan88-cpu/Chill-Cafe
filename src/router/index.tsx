ae61f6d0ccc49176838810b1ae0ce6735782807e
ryan88-cpu <ryansupit88@gmail.com>
2025-11-11 10:04:38 +0800

    feat: slicing home done and navigation to MENU drink and food

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Pages from '../pages'; // default import from pages index
 
-const { SplashScreen, SignIn, SignUp, Home } = Pages;
+const { SplashScreen, SignIn, SignUp, Home, Menu } = Pages;
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
@@ -12,6 +12,7 @@ export default function Router() {
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Menu" component={Menu} />
     </Stack.Navigator>
   );
 }
